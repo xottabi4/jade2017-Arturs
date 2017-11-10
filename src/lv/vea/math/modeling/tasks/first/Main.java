d757c2853a81156ad4acf85e232ee4b4c7f922ba
Arturs Anohins <arturs.anohins@gmail.com>
2017-11-10 11:12:32 +0200

    first tast: fixed fibonacci for 0 and 1 parameters

--- src/lv/vea/math/modeling/tasks/first/Main.java
+++ src/lv/vea/math/modeling/tasks/first/Main.java
@@ -4,14 +4,19 @@ import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println(Arrays.toString(getFibonacciSeries(10)));
+		System.out.println(Arrays.toString(getFibonacciSeries(2)));
 	}
 
 	private static int[] getFibonacciSeries(int count) {
 		int[] fibonacciSeries= new int[count];
 
-		fibonacciSeries[0]=1;
-		fibonacciSeries[1]=1;
+		if(count==1){
+			fibonacciSeries[0]=1;
+		}else if(count>=2){
+			fibonacciSeries[0]=1;
+			fibonacciSeries[1]=1;
+		}
+
 		for (int i = 2; i < fibonacciSeries.length; i++) {
 			fibonacciSeries[i]=fibonacciSeries[i-1]+fibonacciSeries[i-2];
 		}
